2024a6453d4481032b603dec630a7bc7e5c7cc21
Gatij Taranekar <gatij.taranekar@gmail.com>
2022-08-13 01:53:41 +0530

    chore: removed console statements

--- src/hooks/useAPIResolver.js
+++ src/hooks/useAPIResolver.js
@@ -2,19 +2,14 @@ import { useEffect, useState } from 'react';
 import getData from '../utils/getData';
 
 const useAPIResolver = () => {
-    const [isLoading, setIsLoading] = useState(false);
     const [error, setError] = useState('');
     const [cardsGroups, setCardGroups] = useState([]);
     const [onRefresh, setOnRefresh] = useState(false);
 
-    console.log(isLoading);
-
     useEffect(() => {
         getData().then((res) => {
-            setIsLoading(true);
             setError('');
             setCardGroups(res?.card_groups);
-            setIsLoading(false);
         }).catch(() => {
             setError('Something went wrong! Please try again later.');
         });
